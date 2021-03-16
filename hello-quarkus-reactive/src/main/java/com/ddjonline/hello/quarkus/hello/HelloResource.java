4fc3a6c8f47de121ec413d7e290171db6d1a1efe
Darren Jones <ddjones@follett.com>
2021-03-16 18:04:33 -0400

    quarkus reduce to single resource class

--- hello-quarkus-reactive/src/main/java/com/ddjonline/hello/quarkus/hello/HelloResource.java
+++ hello-quarkus-reactive/src/main/java/com/ddjonline/hello/quarkus/hello/HelloResource.java
@@ -22,4 +22,37 @@ public class HelloResource {
     public Uni<String> hello() {
         return Uni.createFrom().item("Hello (" + counter.incrementAndGet() + ")");
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/naptime")
+    public Uni<String> naptime() {
+        return pi_digits(20000);
+    }
+
+    private static Uni<String> pi_digits(int digits) {
+        return Uni.createFrom().emitter(em -> {
+            int scale = 10000;
+            int array_init = 2000;
+            StringBuffer pi = new StringBuffer();
+            int[] arr = new int[digits + 1];
+            int carry = 0;
+    
+            for (int i = 0; i <= digits; ++i)
+                arr[i] = array_init;
+    
+            for (int i = digits; i > 0; i -= 14) {
+                int sum = 0;
+                for (int j = i; j > 0; --j) {
+                    sum = sum * j + scale * arr[j];
+                    arr[j] = sum % (j * 2 - 1);
+                    sum /= j * 2 - 1;
+                }
+    
+                pi.append(String.format("%04d", carry + sum / scale));
+                carry = sum % scale;
+            }
+            em.complete("your slice of pi is " + pi.toString());
+        });
+    }
 }
--- hello-quarkus/src/main/java/com/ddjonline/hello/quarkus/hello/HelloResource.java
+++ hello-quarkus/src/main/java/com/ddjonline/hello/quarkus/hello/HelloResource.java
@@ -20,4 +20,36 @@ public class HelloResource {
     public String hello() {
         return "Hello (" + counter.incrementAndGet() + ")";
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/naptime")
+    public String naptime() {
+        return pi_digits(20000);
+    }
+
+    private static String pi_digits(int digits) {
+
+        int scale = 10000;
+        int array_init = 2000;
+        StringBuffer pi = new StringBuffer();
+        int[] arr = new int[digits + 1];
+        int carry = 0;
+
+        for (int i = 0; i <= digits; ++i)
+            arr[i] = array_init;
+
+        for (int i = digits; i > 0; i -= 14) {
+            int sum = 0;
+            for (int j = i; j > 0; --j) {
+                sum = sum * j + scale * arr[j];
+                arr[j] = sum % (j * 2 - 1);
+                sum /= j * 2 - 1;
+            }
+
+            pi.append(String.format("%04d", carry + sum / scale));
+            carry = sum % scale;
+        }
+        return "your slice of pi is " + pi.toString();
+    }
 }
