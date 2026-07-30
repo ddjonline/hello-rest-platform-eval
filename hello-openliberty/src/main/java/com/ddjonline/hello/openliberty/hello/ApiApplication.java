a0f81d67c1ef3f271b08e2a39589bc78e92607bd
Darren Jones <ddjonline@gmail.com>
2026-07-29 22:53:17 -0400

    update hello-openliberty

--- hello-openliberty/src/main/java/com/ddjonline/hello/openliberty/hello/ApiApplication.java
+++ hello-openliberty/src/main/java/com/ddjonline/hello/openliberty/hello/ApiApplication.java
@@ -1,10 +1,10 @@
 package com.ddjonline.hello.openliberty.hello;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class ApiApplication extends Application
 {
-    
+
 }
--- hello-openliberty/src/main/java/com/ddjonline/hello/openliberty/hello/HelloResource.java
+++ hello-openliberty/src/main/java/com/ddjonline/hello/openliberty/hello/HelloResource.java
@@ -2,11 +2,11 @@ package com.ddjonline.hello.openliberty.hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @RequestScoped
 @Path("hello")
